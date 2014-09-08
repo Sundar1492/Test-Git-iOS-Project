a308f372eb596da2ff4c6ae125aa14e8fd096d5e
Sundar Seshadri <sundar@ninjaas.com>
2014-09-08 15:48:07 +0530

    added a test class

---
+++ Git Test/SSTest.h
@@ -0,0 +1,13 @@
+//
+//  SSTest.h
+//  Git Test
+//
+//  Created by Seshadri Sundaram on 08/09/14.
+//  Copyright (c) 2014 Seshadrins. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SSTest : NSObject
+
+@end
---
+++ Git Test/SSTest.m
@@ -0,0 +1,13 @@
+//
+//  SSTest.m
+//  Git Test
+//
+//  Created by Seshadri Sundaram on 08/09/14.
+//  Copyright (c) 2014 Seshadrins. All rights reserved.
+//
+
+#import "SSTest.h"
+
+@implementation SSTest
+
+@end
